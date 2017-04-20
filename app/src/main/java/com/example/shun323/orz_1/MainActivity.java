23eeaa59ca4d38cd0671f2ac2b4abf9d26a66de8
Episword <mistio100@gmail.com>
2017-04-20 20:34:38 +0900

    onClickListener

--- app/src/main/java/com/example/shun323/orz_1/MainActivity.java
+++ app/src/main/java/com/example/shun323/orz_1/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.shun323.orz_1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,7 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        }
+
+        //ルールボタン
+        Button btn_rule = (Button) findViewById(R.id.button2);
+        btn_rule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.layout);
+            }
+        });
     }
+
+}
 
 
